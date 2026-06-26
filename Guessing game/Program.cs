d536d851bf854b720aac5af91193babc4ab5d3f9
Pedro Santos <phcastroestudoss@gmail.com>
2026-06-26 15:32:15 -0300

    Adding Guessing Game

---
+++ Guessing game/Program.cs
@@ -0,0 +1,49 @@
+﻿Random random = new Random();
+bool playAgain = true;
+int min = 1;
+int max = 100;
+int guess;
+int number;
+int guesses;
+String? response;
+
+while (playAgain)
+{
+    guess = 0;
+    guesses = 0;
+    number = random.Next(min, max+1);
+    response = "";
+
+    while(guess != number)
+    {
+        System.Console.Write("Guess a number between " + min + " - " + max + " : ");
+        guess = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Guess: " + guess);
+
+        if (guess > number)
+        {
+            System.Console.WriteLine(guess + " is too high!");
+        } else if (guess < number)
+        {
+            System.Console.WriteLine(guess + " is too low!");
+        }
+        guesses++;
+    }
+    System.Console.WriteLine("Number: " + number);
+    System.Console.WriteLine("YOU WIN!");
+    System.Console.WriteLine("Guesses: " + guesses);
+
+    System.Console.Write("Would you like do play again? (Y/N): ");
+    response = Console.ReadLine();
+    response = response?.ToUpper();
+
+    if(response == "Y")
+    {
+        playAgain = true;
+    } else
+    {
+        playAgain = false;
+    }
+}
+
+System.Console.WriteLine("Thanks for playing!");
