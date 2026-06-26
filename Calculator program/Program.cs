6393ecc4d7b7b63919874b53c85840adb05f0157
Pedro Santos <phcastroestudoss@gmail.com>
2026-06-26 16:12:08 -0300

    Adding calculator program

---
+++ Calculator program/Program.cs
@@ -0,0 +1,40 @@
+﻿double num1 = 0;
+double num2 = 0;
+double result = 0;
+
+System.Console.WriteLine("====================");
+System.Console.WriteLine("=Calculator Program=");
+System.Console.WriteLine("====================");
+
+System.Console.Write("Enter number 1: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Enter number 2: ");
+num2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Enter an option: \n+ : Add\n- : Substract\n* : Multiply\n/ : Divide \n");
+
+switch(Console.ReadLine())
+{
+    case "+":
+        result = num1 + num2;
+        System.Console.WriteLine($"Your result: {num1} + {num2} = {result}");
+        break;
+
+    case "-":
+        result = num1 - num2;
+        System.Console.WriteLine($"Your result: {num1} - {num2} = {result}");
+        break;
+
+    case "*":
+        result = num1 * num2;
+        System.Console.WriteLine($"Your result: {num1} * {num2} = {result}");
+        break;
+
+    case "/":
+        result = num1 / num2;
+        System.Console.WriteLine($"Your result: {num1} / {num2} = {result}");
+        break;
+    
+    default:
+        System.Console.WriteLine("That was not a valid option");
+        break;
+};
